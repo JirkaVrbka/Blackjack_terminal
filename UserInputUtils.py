2ebb094c2bd4541436f5d5c19cfd8843d7eedc5e
Jirka <jiri.vrbka@gmail.com>
2019-07-01 22:47:54 +0200

    Utils for handling user inputs

---
+++ UserInputUtils.py
@@ -0,0 +1,28 @@
+
+def get_int(msg=""):
+    while True:
+        try:
+            val = int(input(msg+"\n"))
+            return val
+        except ValueError:
+            print("Unable to convert you value into number, try one more time")
+
+
+def get_int_with_default(default, msg=""):
+    while True:
+        try:
+            val = int(input(msg+"\n"))
+            return val
+        except ValueError:
+            print("This is not a number! I set it for you {}".format(default))
+
+
+def get_positive_int_with_default(default, msg=""):
+    while True:
+        try:
+            val = int(input(msg+"\n"))
+            if val >= 0:
+                return val
+        except ValueError:
+            pass
+        print("Unable to convert you value into number, so I set for you {}".format(default))
